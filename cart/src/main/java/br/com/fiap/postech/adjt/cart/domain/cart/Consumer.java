2f731bdf79a3c9ce81d1ae0e915f672006303edd
Gustavo Reche <gustavodreche@msn.com>
2024-09-22 11:57:43 -0300

    Ajusta log de error

--- cart/src/main/java/br/com/fiap/postech/adjt/cart/domain/cart/Consumer.java
+++ cart/src/main/java/br/com/fiap/postech/adjt/cart/domain/cart/Consumer.java
@@ -1,20 +1,25 @@
 package br.com.fiap.postech.adjt.cart.domain.cart;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Objects;
 import java.util.UUID;
 
+@Slf4j
 public record Consumer(
         String consumerId
 ) {
 
     public Consumer {
         if (Objects.isNull(consumerId)) {
-            throw new IllegalArgumentException("EAN NAO PODE SER NULO OU MENOR E IGUAL A ZERO!");
+            log.error("consumerId NAO PODE SER NULO");
+            throw new IllegalArgumentException("consumerId NAO PODE SER NULO");
         }
         try {
             UUID.fromString(consumerId).toString().equals(consumerId);
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("EAN NAO PODE SER NULO OU MENOR E IGUAL A ZERO!");
+            log.error("consumerId TEM QUE TER O FORMATO DE UUID");
+            throw new IllegalArgumentException("consumerId TEM QUE TER O FORMATO DE UUID");
         }
     }
 
