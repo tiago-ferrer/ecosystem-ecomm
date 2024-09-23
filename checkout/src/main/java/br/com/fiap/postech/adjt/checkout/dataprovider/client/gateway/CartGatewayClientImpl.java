e8c67fc3effbcd5ef50b8aad92dc682d1cf26b25
Diego Nassula <diego.nassula@digihub.com.br>
2024-09-23 19:33:02 -0300

    Criando integração com o carrinho

--- checkout/src/main/java/br/com/fiap/postech/adjt/checkout/dataprovider/client/gateway/CartGatewayClientImpl.java
+++ checkout/src/main/java/br/com/fiap/postech/adjt/checkout/dataprovider/client/gateway/CartGatewayClientImpl.java
@@ -1,19 +1,43 @@
 package br.com.fiap.postech.adjt.checkout.dataprovider.client.gateway;
 
 import br.com.fiap.postech.adjt.checkout.domain.gateway.CartGateway;
-import br.com.fiap.postech.adjt.checkout.domain.model.cart.CartItensModel;
 import br.com.fiap.postech.adjt.checkout.domain.model.cart.CartModel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class CartGatewayClientImpl implements CartGateway {
+
+    @Value("${app.cart-base-url}")
+    private String cartBaseUrl;
+
     @Override
     public CartModel getCartByConsumerId(String consumerId) {
-        /*
-            TODO criar integracao com carrinho
-         */
-        return new CartModel(List.of(new CartItensModel("137453ac-21c3-4e43-a652-7e2867fcb6d1", 1L)));
+
+        RestTemplate client = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", "application/json");
+        headers.set("Custom-Header", "custom-header-value");
+
+        String body = "{ \"consumerId\": \"" + consumerId + "\" }";
+
+        HttpEntity<String> entity = new HttpEntity<>(body, headers);
+
+        try {
+            ResponseEntity<CartModel> response = client.exchange(cartBaseUrl, HttpMethod.GET, entity, CartModel.class);
+            if (response.getStatusCode().is2xxSuccessful()) {
+                return response.getBody();
+            } else {}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
+
 }
