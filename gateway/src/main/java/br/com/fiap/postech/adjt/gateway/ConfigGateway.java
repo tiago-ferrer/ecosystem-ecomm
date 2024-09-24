0808a4bced1845bd5c40daa745e466de9378a9c8
Rafael Amaral Naves Avelar <amaral.avelar.filo@gmail.com>
2024-09-24 02:44:34 -0300

    feat: 400 response without product

--- gateway/src/main/java/br/com/fiap/postech/adjt/gateway/ConfigGateway.java
+++ gateway/src/main/java/br/com/fiap/postech/adjt/gateway/ConfigGateway.java
@@ -3,6 +3,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import reactor.core.publisher.Mono;
 
 @Configuration
@@ -19,10 +20,16 @@ public class ConfigGateway {
                         .and()
                         .not( p -> p.path("/checkout/api/**"))
                         .filters(f -> f)
-                        .uri("http://localhost:8082"))
+                        .uri("http://checkout:8082"))
                 .route("products", r -> r.path("/products/**")
                         .filters(f -> f.modifyResponseBody(String.class, String.class,
-                                (exchange, s) -> Mono.just(s.toUpperCase()))).uri("https://fakestoreapi.com"))
+                                (exchange, s) -> {
+                               if (s == null) {
+                                 exchange.getResponse().setStatusCode(HttpStatus.BAD_REQUEST);
+                                 return Mono.just("Id não localizado");
+                               };
+                              return Mono.just(s);
+                                })).uri("https://fakestoreapi.com"))
                 .build();
     }
 }
