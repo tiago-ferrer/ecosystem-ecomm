f3cff71021ded4991a35c15d0e7329be5d10c4d6
Barreto <v.barreto.and@gmail.com>
2024-09-25 08:44:07 -0300

    feat: add cart and checkout redirection

--- gateway/src/main/java/br/com/fiap/postech/adjt/gateway/ConfigGateway.java
+++ gateway/src/main/java/br/com/fiap/postech/adjt/gateway/ConfigGateway.java
@@ -10,12 +10,20 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ConfigGateway {
 
+    @Value("${api.cart.url}")
+    private String CART_URL;
+
+    @Value("${api.checkout.url}")
+    private String CHECKOUT_URL;
+
     @Value("${api.products.url}")
     private String PRODUCTS_URL;
 
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
+                .route("cart", r -> r.path("/cart/**").uri(CART_URL))
+                .route("checkout", r -> r.path("/checkout/**").uri(CHECKOUT_URL))
                 .route("product", r -> r.path("/products/**").uri(PRODUCTS_URL))
                 .build();
     }
