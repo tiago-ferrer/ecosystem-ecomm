e1b3b6b9525fb7b7295b8607b720384c335cac13
Danizin <dani.rmartins@hotmail.com>
2024-09-26 19:47:54 -0300

    #arrumando para em prod chamar o catalog.

--- cart/src/main/java/br/com/fiap/postech/adjt/cart/feign/ProductClient.java
+++ cart/src/main/java/br/com/fiap/postech/adjt/cart/feign/ProductClient.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "product-client", url = "https://fakestoreapi.com")
+@FeignClient(name = "product-client",  url = "${product-client.urlProduct}")
 public interface ProductClient {
-    @GetMapping("/products/{itemId}")
+    @GetMapping("/{itemId}")
     ProductDto getProductById(@PathVariable("itemId") Long itemId);
 }
