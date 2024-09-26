16bc60bbb23cdd6cca60eaff4ef2f9bc1532099c
Barreto <v.barreto.and@gmail.com>
2024-09-26 19:03:45 -0300

    feat: move implementation details to impl class

--- checkout/src/main/java/br/com/fiap/postech/adjt/checkout/controller/CheckoutController.java
+++ checkout/src/main/java/br/com/fiap/postech/adjt/checkout/controller/CheckoutController.java
@@ -1,41 +1,22 @@
 package br.com.fiap.postech.adjt.checkout.controller;
 
-import java.util.List;
-import java.util.UUID;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import br.com.fiap.postech.adjt.checkout.model.OrderEntity;
 import br.com.fiap.postech.adjt.checkout.model.request.CheckoutRequest;
 import br.com.fiap.postech.adjt.checkout.model.response.CheckoutResponse;
 import br.com.fiap.postech.adjt.checkout.model.response.OrderCheckoutsResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@RequestMapping("/checkout")
+import java.util.List;
+import java.util.UUID;
+
 public interface CheckoutController {
 
-    @Operation(
-            summary = "Executa o pagamento",
-            description = "Endpoint voltado pagamento de Carrinhos de Compras que estejam integros para conclusão..",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "ok."),
-                    @ApiResponse(responseCode = "400", description = "Bad request."),
-            }
-    )
-    @PostMapping("/")
     ResponseEntity<CheckoutResponse> createCheckout(@Valid @RequestBody CheckoutRequest checkoutRequest);
 
-    @GetMapping("/{consumerId}")
     ResponseEntity<List<OrderCheckoutsResponse>> getOrdersByConsumerId(@PathVariable UUID consumerId);
-    
-    @GetMapping("/by-order-id/{orderId}")
-    ResponseEntity<OrderEntity> getOrderById(@PathVariable UUID orderId);
 
+    ResponseEntity<OrderEntity> getOrderById(@PathVariable UUID orderId);
 }
--- checkout/src/main/java/br/com/fiap/postech/adjt/checkout/controller/impl/CheckoutControllerImpl.java
+++ checkout/src/main/java/br/com/fiap/postech/adjt/checkout/controller/impl/CheckoutControllerImpl.java
@@ -1,20 +1,25 @@
 package br.com.fiap.postech.adjt.checkout.controller.impl;
 
-import java.util.List;
-import java.util.UUID;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-
 import br.com.fiap.postech.adjt.checkout.controller.CheckoutController;
 import br.com.fiap.postech.adjt.checkout.model.OrderEntity;
 import br.com.fiap.postech.adjt.checkout.model.request.CheckoutRequest;
 import br.com.fiap.postech.adjt.checkout.model.response.CheckoutResponse;
 import br.com.fiap.postech.adjt.checkout.model.response.OrderCheckoutsResponse;
 import br.com.fiap.postech.adjt.checkout.service.CheckoutService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/checkout")
 public class CheckoutControllerImpl implements CheckoutController {
 
     private final CheckoutService checkoutService;
@@ -24,29 +29,40 @@ public class CheckoutControllerImpl implements CheckoutController {
         this.checkoutService = checkoutService;
     }
 
+
     @Override
+    @Operation(
+            summary = "Executa o pagamento",
+            description = "Endpoint voltado pagamento de Carrinhos de Compras que estejam integros para conclusão..",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "ok."),
+                    @ApiResponse(responseCode = "400", description = "Bad request."),
+            }
+    )
+    @PostMapping
     public ResponseEntity<CheckoutResponse> createCheckout(CheckoutRequest checkoutRequest) {
         checkoutRequest.validateConsumerId();
 
         CheckoutResponse checkoutResponse = checkoutService.processCheckout(
-        		UUID.fromString(checkoutRequest.getConsumerId()),
-        		checkoutRequest.getAmount(),
-        		checkoutRequest.getCurrency(),
-        		checkoutRequest.getPaymentMethod()
+                UUID.fromString(checkoutRequest.getConsumerId()),
+                checkoutRequest.getAmount(),
+                checkoutRequest.getCurrency(),
+                checkoutRequest.getPaymentMethod()
         );
-                return ResponseEntity.ok(checkoutResponse);
+        return ResponseEntity.ok(checkoutResponse);
     }
 
     @Override
+    @GetMapping("/{consumerId}")
     public ResponseEntity<List<OrderCheckoutsResponse>> getOrdersByConsumerId(UUID consumerId) {
         List<OrderCheckoutsResponse> orders = checkoutService.getOrdersByConsumerId(consumerId);
         return ResponseEntity.ok(orders);
     }
-    
+
     @Override
+    @GetMapping("/by-order-id/{orderId}")
     public ResponseEntity<OrderEntity> getOrderById(UUID orderId) {
         OrderEntity order = checkoutService.getOrderById(orderId);
         return ResponseEntity.ok(order);
     }
-    
 }
