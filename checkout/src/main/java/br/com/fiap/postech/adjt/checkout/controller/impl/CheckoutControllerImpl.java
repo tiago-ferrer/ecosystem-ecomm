cb9aa7e10879b178ac550038bd5cabe2ceef8de7
Barreto <v.barreto.and@gmail.com>
2024-09-27 12:11:04 -0300

    chore: organize imports

--- checkout/src/main/java/br/com/fiap/postech/adjt/checkout/controller/impl/CheckoutControllerImpl.java
+++ checkout/src/main/java/br/com/fiap/postech/adjt/checkout/controller/impl/CheckoutControllerImpl.java
@@ -10,10 +10,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
