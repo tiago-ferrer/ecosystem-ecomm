6c7e2e474bd0bfc16d13ecc627d7d20f73c81d69
Barreto <v.barreto.and@gmail.com>
2024-09-26 22:00:16 -0300

    feat: add setter of id

--- cart/src/main/java/br/com/fiap/postech/adjt/cart/model/entity/Cart.java
+++ cart/src/main/java/br/com/fiap/postech/adjt/cart/model/entity/Cart.java
@@ -31,6 +31,10 @@ public class Cart {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public UUID getCustomerId() {
         return customerId;
     }
