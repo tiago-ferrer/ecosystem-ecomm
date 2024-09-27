eded678cedf8c11f5534710c05dd8ef4d9ec709c
Barreto <v.barreto.and@gmail.com>
2024-09-27 12:10:39 -0300

    feat: use restclient from cart requests instead of openfeign
    Glr, eu lembro que a gente combinou de usar o openfeign como padrão mas a especificação ta meio torta (ex: GET com body). Tem como interceptar a requisição do openfeign em ambos os modulos pra seguir o contrato mas como o tempo ta curto e é trabalhoso demais para burlar o REST: o crime não compensa.

--- checkout/src/main/java/br/com/fiap/postech/adjt/checkout/clients/CartClient.java
+++ checkout/src/main/java/br/com/fiap/postech/adjt/checkout/clients/CartClient.java
@@ -3,16 +3,10 @@ package br.com.fiap.postech.adjt.checkout.clients;
 import br.com.fiap.postech.adjt.checkout.model.dto.request.ClearCartRequest;
 import br.com.fiap.postech.adjt.checkout.model.dto.request.FindCartByCustomerIdRequest;
 import br.com.fiap.postech.adjt.checkout.model.dto.response.CartResponse;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import br.com.fiap.postech.adjt.checkout.model.dto.response.MessageResponse;
 
-@FeignClient(name = "cartClient", url = "${cart.service.url}/cart")
 public interface CartClient {
-    @GetMapping
-    CartResponse consult(@RequestBody FindCartByCustomerIdRequest request);
+    CartResponse consult(FindCartByCustomerIdRequest request);
 
-    @DeleteMapping
-    void clear(@RequestBody ClearCartRequest request);
+    MessageResponse clear(ClearCartRequest request);
 }
