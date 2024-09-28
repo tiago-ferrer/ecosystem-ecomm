aeb8f56ff831213a81c8c6953bd9ba14247cc678
Rafael Amaral Naves Avelar <amaral.avelar.filo@gmail.com>
2024-09-27 22:23:35 -0300

    feat: testes

--- checkout/src/main/java/br/com/fiap/postech/adjt/checkout/infrastructure/persistance/entities/ItemEntity.java
+++ checkout/src/main/java/br/com/fiap/postech/adjt/checkout/infrastructure/persistance/entities/ItemEntity.java
@@ -1,8 +1,9 @@
 package br.com.fiap.postech.adjt.checkout.infrastructure.persistance.entities;
 
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -18,8 +19,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ItemEntity {
     @Id
-    @ManyToOne
     private Integer itemId;
-
     private Integer quantity;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false) // Define a chave estrangeira
+    private OrderEntity order;
 }
--- checkout/src/main/java/br/com/fiap/postech/adjt/checkout/infrastructure/persistance/entities/OrderEntity.java
+++ checkout/src/main/java/br/com/fiap/postech/adjt/checkout/infrastructure/persistance/entities/OrderEntity.java
@@ -1,12 +1,24 @@
 package br.com.fiap.postech.adjt.checkout.infrastructure.persistance.entities;
 
-import br.com.fiap.postech.adjt.checkout.domain.entities.enums.OrderStatus;
-import jakarta.persistence.*;
-import lombok.*;
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.List;
 import java.util.UUID;
+
+import br.com.fiap.postech.adjt.checkout.domain.entities.enums.OrderStatus;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 @Table(name = "orders")
 @Entity(name = "orders")
@@ -20,9 +32,9 @@ public class OrderEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    @Column(name="consumer_id")
+    @Column(name = "consumer_id")
     private UUID consumerId;
-    @OneToMany(mappedBy = "itemId")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<ItemEntity> items;
     Integer value;
     @Column(name = "payment_type")
