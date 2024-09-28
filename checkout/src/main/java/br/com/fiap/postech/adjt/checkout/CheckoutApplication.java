e39e6b32efed852a2859099578f993d564d1e79e
Rafael Amaral Naves Avelar <amaral.avelar.filo@gmail.com>
2024-09-27 23:01:58 -0300

    more tests

--- checkout/src/main/java/br/com/fiap/postech/adjt/checkout/CheckoutApplication.java
+++ checkout/src/main/java/br/com/fiap/postech/adjt/checkout/CheckoutApplication.java
@@ -2,10 +2,10 @@ package br.com.fiap.postech.adjt.checkout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@FeignClient
+@EnableFeignClients
 public class CheckoutApplication {
 
     public static void main(String[] args) {
